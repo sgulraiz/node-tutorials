8fb3b266afd3f379be9966cf62de300fcc3275b6
Sheharyar Gulraiz <sgulraiz@Sheharyars-MacBook-Pro.local>
2018-12-25 22:24:16 -0500

    Added route parameters and handling get requests

--- express-intro/app.js
+++ express-intro/app.js
@@ -8,10 +8,36 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-//endpoint to return an array of courses
+//Define courses array
+var courses = [
+    {id: 1, name: 'Course1'},
+    {id: 2, name: 'Course2'},
+    {id: 3, name: 'Course3'},
+]
+//endpoint to return courses
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 })
+//route parameters to within the body of the endpoint - example below we have 
+//a parameter defined in the endpoint named "id" and it could be anything and doesnt have to be just "id"
+//app.get('/api/courses/:id', (req, res) => {
+//    res.send(req.params.id);
+//});
+//Below we will define a get endpoint to return a specific course
+//we will go through the courses array by using the find function and match the id with the id passed in as a parameter in the url
+//if found then we will return the course otherwise we will return http error 404
+app.get('/api/courses/:id', (req, res) => {
+    var course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        res.status(404).send('The course with given id was not found');
+    } else {
+        res.send(course);
+    }
+});
+//in the example below we have 2 parameters defined which can read from the req object and send it as a response
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+});
 //app object also comes with a web server that we can configure to listen a specific port
 //this is hardcoded port - in real world we need to use environment variable to use the port dynamilcally assigned by the hosting provider
 //app.listen(3000, () => console.log('Listening on port 3000...'));
